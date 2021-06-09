13cff9436b4bc986e547e8f61c6b5a3131b695f8
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-06-09 11:39:49 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SobreCargaMetodos_G3_2021_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbSumando1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbSumando2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbSuma1 = new System.Windows.Forms.TextBox();
+            this.btnSumar1 = new System.Windows.Forms.Button();
+            this.txtbSumando12 = new System.Windows.Forms.TextBox();
+            this.txtbSumando22 = new System.Windows.Forms.TextBox();
+            this.txtbSuma2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSumar2 = new System.Windows.Forms.Button();
+            this.txtbSumando32 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbSumando1
+            // 
+            this.txtbSumando1.Location = new System.Drawing.Point(42, 188);
+            this.txtbSumando1.Name = "txtbSumando1";
+            this.txtbSumando1.Size = new System.Drawing.Size(151, 35);
+            this.txtbSumando1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "+";
+            // 
+            // txtbSumando2
+            // 
+            this.txtbSumando2.Location = new System.Drawing.Point(330, 188);
+            this.txtbSumando2.Name = "txtbSumando2";
+            this.txtbSumando2.Size = new System.Drawing.Size(150, 35);
+            this.txtbSumando2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "=";
+            // 
+            // txtbSuma1
+            // 
+            this.txtbSuma1.Location = new System.Drawing.Point(591, 190);
+            this.txtbSuma1.Name = "txtbSuma1";
+            this.txtbSuma1.Size = new System.Drawing.Size(150, 35);
+            this.txtbSuma1.TabIndex = 0;
+            // 
+            // btnSumar1
+            // 
+            this.btnSumar1.Location = new System.Drawing.Point(804, 188);
+            this.btnSumar1.Name = "btnSumar1";
+            this.btnSumar1.Size = new System.Drawing.Size(148, 37);
+            this.btnSumar1.TabIndex = 2;
+            this.btnSumar1.Text = "Sumar";
+            this.btnSumar1.UseVisualStyleBackColor = true;
+            this.btnSumar1.Click += new System.EventHandler(this.btnSumar1_Click);
+            // 
+            // txtbSumando12
+            // 
+            this.txtbSumando12.Location = new System.Drawing.Point(42, 270);
+            this.txtbSumando12.Name = "txtbSumando12";
+            this.txtbSumando12.Size = new System.Drawing.Size(99, 35);
+            this.txtbSumando12.TabIndex = 0;
+            // 
+            // txtbSumando22
+            // 
+            this.txtbSumando22.Location = new System.Drawing.Point(238, 275);
+            this.txtbSumando22.Name = "txtbSumando22";
+            this.txtbSumando22.Size = new System.Drawing.Size(93, 35);
+            this.txtbSumando22.TabIndex = 0;
+            // 
+            // txtbSuma2
+            // 
+            this.txtbSuma2.Location = new System.Drawing.Point(591, 272);
+            this.txtbSuma2.Name = "txtbSuma2";
+            this.txtbSuma2.Size = new System.Drawing.Size(150, 35);
+            this.txtbSuma2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 30);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "=";
+            // 
+            // btnSumar2
+            // 
+            this.btnSumar2.Location = new System.Drawing.Point(804, 270);
+            this.btnSumar2.Name = "btnSumar2";
+            this.btnSumar2.Size = new System.Drawing.Size(148, 37);
+            this.btnSumar2.TabIndex = 2;
+            this.btnSumar2.Text = "Sumar";
+            this.btnSumar2.UseVisualStyleBackColor = true;
+            this.btnSumar2.Click += new System.EventHandler(this.btnSumar2_Click);
+            // 
+            // txtbSumando32
+            // 
+            this.txtbSumando32.Location = new System.Drawing.Point(402, 275);
+            this.txtbSumando32.Name = "txtbSumando32";
+            this.txtbSumando32.Size = new System.Drawing.Size(93, 35);
+            this.txtbSumando32.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 30);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 824);
+            this.Controls.Add(this.btnSumar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSumar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbSuma2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbSumando32);
+            this.Controls.Add(this.txtbSumando22);
+            this.Controls.Add(this.txtbSuma1);
+            this.Controls.Add(this.txtbSumando12);
+            this.Controls.Add(this.txtbSumando2);
+            this.Controls.Add(this.txtbSumando1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbSumando1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbSumando2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbSuma1;
+        private System.Windows.Forms.Button btnSumar1;
+        private System.Windows.Forms.TextBox txtbSumando12;
+        private System.Windows.Forms.TextBox txtbSumando22;
+        private System.Windows.Forms.TextBox txtbSuma2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSumar2;
+        private System.Windows.Forms.TextBox txtbSumando32;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SobreCargaMetodos_G3_2021_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private float Sumar(float sumando1, float sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+
+        private float Sumar(float sumando1, float sumando2, float sumando3)
+        {
+            return sumando1 + sumando2+sumando3;
+        }
+
+        private void btnSumar1_Click(object sender, EventArgs e)
+        {
+            float suma = Sumar( float.Parse( txtbSumando1.Text) , float.Parse( txtbSumando2.Text));
+            txtbSuma1.Text = suma.ToString();
+
+        }
+
+        private void btnSumar2_Click(object sender, EventArgs e)
+        {
+            float suma = Sumar(float.Parse(txtbSumando12.Text), float.Parse(txtbSumando22.Text),float.Parse(txtbSumando32.Text));
+            txtbSuma2.Text = suma.ToString();
+
+        }
+
+
+    }
+}
