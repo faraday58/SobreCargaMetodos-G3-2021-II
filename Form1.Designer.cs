8e37920e940128c526a7bbef17246d63a2c27cb4
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-06-11 11:45:48 -0500

    Arreglos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,7 +50,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // txtbSumando1
             // 
-            this.txtbSumando1.Location = new System.Drawing.Point(42, 188);
+            this.txtbSumando1.Location = new System.Drawing.Point(56, 410);
             this.txtbSumando1.Name = "txtbSumando1";
             this.txtbSumando1.Size = new System.Drawing.Size(151, 35);
             this.txtbSumando1.TabIndex = 0;
@@ -58,7 +58,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(238, 193);
+            this.label1.Location = new System.Drawing.Point(252, 415);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 30);
             this.label1.TabIndex = 1;
@@ -66,7 +66,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // txtbSumando2
             // 
-            this.txtbSumando2.Location = new System.Drawing.Point(330, 188);
+            this.txtbSumando2.Location = new System.Drawing.Point(344, 410);
             this.txtbSumando2.Name = "txtbSumando2";
             this.txtbSumando2.Size = new System.Drawing.Size(150, 35);
             this.txtbSumando2.TabIndex = 0;
@@ -74,7 +74,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(526, 193);
+            this.label2.Location = new System.Drawing.Point(540, 415);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 30);
             this.label2.TabIndex = 1;
@@ -82,14 +82,14 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // txtbSuma1
             // 
-            this.txtbSuma1.Location = new System.Drawing.Point(591, 190);
+            this.txtbSuma1.Location = new System.Drawing.Point(605, 412);
             this.txtbSuma1.Name = "txtbSuma1";
             this.txtbSuma1.Size = new System.Drawing.Size(150, 35);
             this.txtbSuma1.TabIndex = 0;
             // 
             // btnSumar1
             // 
-            this.btnSumar1.Location = new System.Drawing.Point(804, 188);
+            this.btnSumar1.Location = new System.Drawing.Point(818, 410);
             this.btnSumar1.Name = "btnSumar1";
             this.btnSumar1.Size = new System.Drawing.Size(148, 37);
             this.btnSumar1.TabIndex = 2;
@@ -99,21 +99,21 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // txtbSumando12
             // 
-            this.txtbSumando12.Location = new System.Drawing.Point(42, 270);
+            this.txtbSumando12.Location = new System.Drawing.Point(56, 492);
             this.txtbSumando12.Name = "txtbSumando12";
             this.txtbSumando12.Size = new System.Drawing.Size(99, 35);
             this.txtbSumando12.TabIndex = 0;
             // 
             // txtbSumando22
             // 
-            this.txtbSumando22.Location = new System.Drawing.Point(238, 275);
+            this.txtbSumando22.Location = new System.Drawing.Point(252, 497);
             this.txtbSumando22.Name = "txtbSumando22";
             this.txtbSumando22.Size = new System.Drawing.Size(93, 35);
             this.txtbSumando22.TabIndex = 0;
             // 
             // txtbSuma2
             // 
-            this.txtbSuma2.Location = new System.Drawing.Point(591, 272);
+            this.txtbSuma2.Location = new System.Drawing.Point(605, 494);
             this.txtbSuma2.Name = "txtbSuma2";
             this.txtbSuma2.Size = new System.Drawing.Size(150, 35);
             this.txtbSuma2.TabIndex = 0;
@@ -121,7 +121,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(176, 275);
+            this.label3.Location = new System.Drawing.Point(190, 497);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 30);
             this.label3.TabIndex = 1;
@@ -130,7 +130,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(526, 275);
+            this.label4.Location = new System.Drawing.Point(540, 497);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(27, 30);
             this.label4.TabIndex = 1;
@@ -138,7 +138,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // btnSumar2
             // 
-            this.btnSumar2.Location = new System.Drawing.Point(804, 270);
+            this.btnSumar2.Location = new System.Drawing.Point(818, 492);
             this.btnSumar2.Name = "btnSumar2";
             this.btnSumar2.Size = new System.Drawing.Size(148, 37);
             this.btnSumar2.TabIndex = 2;
@@ -148,7 +148,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             // txtbSumando32
             // 
-            this.txtbSumando32.Location = new System.Drawing.Point(402, 275);
+            this.txtbSumando32.Location = new System.Drawing.Point(416, 497);
             this.txtbSumando32.Name = "txtbSumando32";
             this.txtbSumando32.Size = new System.Drawing.Size(93, 35);
             this.txtbSumando32.TabIndex = 0;
@@ -156,7 +156,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(350, 278);
+            this.label5.Location = new System.Drawing.Point(364, 500);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(27, 30);
             this.label5.TabIndex = 1;
@@ -192,7 +192,7 @@ namespace SobreCargaMetodos_G3_2021_II
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 411);
+            this.ClientSize = new System.Drawing.Size(1018, 753);
             this.Controls.Add(this.btnArreglo);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.btnSumar2);
